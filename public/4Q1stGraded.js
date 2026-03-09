0daa8d530422811c6da00cf49754387bbac35d3c
carloseeyah <b2029cmrsia@pshs.edu.ph>
2026-03-09 21:15:51 +0800

    4q 1st graded

---
+++ public/4Q1stGraded.js
@@ -0,0 +1,50 @@
+const output = document.getElementById("output");
+const stars = document.querySelectorAll('.star');
+const ratingInput = document.getElementById('ratingInput');
+
+document.getElementById("movieRatingForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const data = Object.fromEntries(new FormData(this).entries());
+
+    let movies = JSON.parse(localStorage.getItem("movies")) || [];
+    movies.push(data);
+    localStorage.setItem("movies", JSON.stringify(movies));
+
+    alert("Movie rating saved successfully!");
+    this.reset();
+
+    let html = '<h2>Movie Ratings:</h2><dl>';
+
+    movies.forEach(s => {
+
+        let noOfStars = `<span class="fa fa-star checked"></span>`.repeat(s.rating);
+
+        html += `<dt> ${s.movieTitle} (${s.movieYear}) - ${s.genre}, Rating: ${noOfStars}</dt>`
+    });
+
+    html += `</dl>`;
+    output.innerHTML = html;
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    stars.forEach(star => {
+        star.addEventListener('click', function() {
+            const rating = this.getAttribute('data-value');
+
+            if (ratingInput) {
+                ratingInput.value = rating;
+            }
+
+            stars.forEach((s, i) => {
+                const span = s.querySelector('.fa');
+                if (i < rating) {
+                    span.classList.add('checked');
+                } else {
+                    span.classList.remove('checked');
+                }
+            });
+        });
+    });
+});
