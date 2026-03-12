219cf29f5e3906de11d47d28d82de98993c0b192
carloseeyah <b2029cmrsia@pshs.edu.ph>
2026-03-12 23:29:13 +0800

    graded exercise 2

--- public/4Q1stGraded.js
+++ public/4Q1stGraded.js
@@ -8,14 +8,12 @@ const titleInput = document.getElementById('movieTitle')
 document.getElementById("movieRatingForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
-
     const data = Object.fromEntries(new FormData(this).entries());
 
     let movies = JSON.parse(localStorage.getItem("movies")) || [];
     movies.push(data);
     localStorage.setItem("movies", JSON.stringify(movies));
 
-    alert("Movie rating saved successfully!");
     this.reset();
 
     let html = '<h2>Movie Ratings:</h2><dl>';
